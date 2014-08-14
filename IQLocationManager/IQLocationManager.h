5ea367032e1a4dd2e96e3382b7eef0f546918429
Nacho Sanchez <nacho.sanchez@inqbarna.com>
2014-08-14 19:40:16 +0200

    Añadiendo las clases que darán sentido a esto

---
+++ IQLocationManager/IQLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  IQLocationManager.h
+//  IQLocationManagerDemo
+//
+//  Created by Nacho Sánchez on 14/08/14.
+//  Copyright (c) 2014 InQBarna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IQLocationManager : NSObject
+
+@end
---
+++ IQLocationManager/IQLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  IQLocationManager.m
+//  IQLocationManagerDemo
+//
+//  Created by Nacho Sánchez on 14/08/14.
+//  Copyright (c) 2014 InQBarna. All rights reserved.
+//
+
+#import "IQLocationManager.h"
+
+@implementation IQLocationManager
+
+@end
